10a2d45e3e7a2fcf3a93aecbd804151a0c4131c2
MohannadMosaad <Mohannadmosaad@gmail.com>
2024-10-23 11:38:14 +0300

    Small Routing Edit

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -4,7 +4,7 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
